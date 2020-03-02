baafd3bb7302d2a67a8638e1a7dc7bc386a7e7e6
JS230310 <JS230310@falmouth.ac.uk>
2020-03-02 11:54:22 +0000

    Added Work on propulsion and steering

--- COMP140GameController/Assets/Scripts/SCR_SteeringScript.cs
+++ COMP140GameController/Assets/Scripts/SCR_SteeringScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SCR_SteeringScript : MonoBehaviour
+
+/*public class SCR_SteeringScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    SerialPort serial = new SerialPort("COM3", 9600);
+    private int Heading;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Heading = int.Parse(serial.ReadLine());
+        transform.localEulerAngles = new Vector3(0, Heading, 0);
     }
-}
+}*/
